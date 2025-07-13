03012ed6377e45f9a8fcb09b7d0b16073e67139f
Anjali846 <anjaliyadav00049@gmail.com>
2025-07-13 23:07:18 +0530

    TodoList.jsx

---
+++ TodoList.jsx
@@ -0,0 +1,18 @@
+import TodoItem from './TodoItem'
+
+function TodoList({ tasks, toggleComplete, deleteTask }) {
+  return (
+    <ul>
+      {tasks.map(task => (
+        <TodoItem
+          key={task.id}
+          task={task}
+          toggleComplete={toggleComplete}
+          deleteTask={deleteTask}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default TodoList
